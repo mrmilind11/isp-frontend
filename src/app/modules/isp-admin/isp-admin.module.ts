8632a3882cb46fbcf2b40c48efb3369a2740b570
Milind Sharma <milind.sharma@searce.com>
2020-02-22 16:02:19 +0530

    Initialisation and material dependency added. Basic structure configured

---
+++ src/app/modules/isp-admin/isp-admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IspAdminRoutingModule } from './isp-admin-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IspAdminRoutingModule
+  ]
+})
+export class IspAdminModule { }
---
+++ src/app/modules/isp-user/isp-user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IspUserRoutingModule } from './isp-user-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IspUserRoutingModule
+  ]
+})
+export class IspUserModule { }
