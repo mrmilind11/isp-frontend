aebcf404bcc6dca4bbda5033918ee0f75d70973b
Milind Sharma <milind.sharma@searce.com>
2020-02-23 23:33:29 +0530

    default isp image directive changes

--- src/app/modules/shared/directives/default-isp-image.directive.ts
+++ src/app/modules/shared/directives/default-isp-image.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[defaultIspImage]'
+  selector: 'img[defaultIspImage]',
+  host: {
+    '(error)': 'setDefaultImage()'
+  }
 })
 export class DefaultIspImageDirective {
-
-  constructor() { }
+  /********************************************** Constructor ***************************************/
+  constructor(
+    private el: ElementRef
+  ) { }
+  /********************************************** Properties ****************************************/
+  private defaultISPImagePath = '../../../../assets/images/default_isp.png';
+  /********************************************** Methods *******************************************/
+  public setDefaultImage() {
+    this.el.nativeElement.src = this.defaultISPImagePath;
+  }
 
 }
