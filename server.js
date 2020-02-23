9bcb229b628dc883662d04f0ee84e8a31ed5e31f
Milind Sharma <milind.sharma@searce.com>
2020-02-24 00:16:49 +0530

    server js path fix

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/musicLibApp'));
+app.use(express.static(__dirname + '/dist/ispApp'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/musicLibApp/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/ispApp/index.html'));
 })
 
 app.listen(process.env.PORT || 3000, () => {
