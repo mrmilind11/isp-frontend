00580e7662091404f1241fd1ffabd1017aa2e254
Milind Sharma <milind.sharma@searce.com>
2020-02-23 12:19:38 +0530

    isp detail sidenav change

--- src/app/modules/isp-user/components/isp-user-container/isp-user-container.component.ts
+++ src/app/modules/isp-user/components/isp-user-container/isp-user-container.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'isp-user-container',
   templateUrl: './isp-user-container.component.html',
   styleUrls: ['./isp-user-container.component.scss']
 })
 export class IspUserContainerComponent implements OnInit {
-
+  /*************************************** Constructor ******************************/
   constructor() { }
-
+  /*************************************** Properties *******************************/
+  private sidenavMode: 'side' | 'over' = 'side';
+  /*************************************** Methods **********************************/
   ngOnInit() {
+    this.setSideNavMode();
+    this.setUpdateListener();
+  }
+  private setUpdateListener() {
+    fromEvent(window, 'resize').pipe(debounceTime(500)).subscribe(() => {
+      this.setSideNavMode();
+    })
+  }
+  private setSideNavMode() {
+    this.sidenavMode = window.screen.width <= 1400 ? 'over' : 'side';
   }
 
 }
